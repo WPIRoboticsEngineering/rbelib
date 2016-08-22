dd489b8a90913aa39e7bca928e5cc0841670c7a4
Joseph St. Germain <joest@wpi.edu>
2016-08-22 13:43:24 -0400

    initial commit -joest

--- A16_Example/Release/SRC/main.d
+++ A16_Example/Release/SRC/main.d
@@ -1,46 +1,46 @@
 SRC/main.d SRC/main.o: ../SRC/main.c \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/RBELib.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/ADC.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/DAC.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/Debug.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/motors.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/USARTDebug.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/timer.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/Periph.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/pot.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/PID.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/reg_structs.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/ports.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SPI.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SetServo.h \
-  R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SlaveSelects.h
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/RBELib.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/ADC.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/DAC.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/Debug.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/motors.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/USARTDebug.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/timer.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/Periph.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/pot.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/PID.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/reg_structs.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/ports.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SPI.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SetServo.h \
+  R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SlaveSelects.h
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/RBELib.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/RBELib.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/ADC.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/ADC.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/DAC.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/DAC.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/Debug.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/Debug.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/motors.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/motors.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/USARTDebug.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/USARTDebug.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/timer.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/timer.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/Periph.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/Periph.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/pot.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/pot.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/PID.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/PID.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/reg_structs.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/reg_structs.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/ports.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/ports.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SPI.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SPI.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SetServo.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SetServo.h:
 
-R:\RBE3001\ example\ workspace\RBELib\include/RBELib/SlaveSelects.h:
+R:\RBE3001\ testing\ example\ workspace\rbelib16-17\RBELib\include/RBELib/SlaveSelects.h:
